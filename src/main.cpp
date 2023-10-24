e9bcd37b7d9a8c7147b9fea984feaa23fce7a132
TomaszKK <242136@edu.p.lodz.pl>
2023-10-24 19:30:36 +0200

    First look of the top bar, without any working functions, only buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <QtWidgets>
 #include "widget/LineEntryWidget.hpp"
+#include "widget/TopBarWidget.hpp"
 
 int main(int argc, char *argv[]){
     QApplication app(argc, argv);
     LOG_INFO("Starting the application");
 
+
+    // LineEntryWidget line(LineEntryWidget::LineType::DIRECTORY, "test", 64);
+    // line.show();
+    TopBarWidget topBar;
+    topBar.show();
     //Loading the main widget
 
     LOG_INFO("The application was loaded");
